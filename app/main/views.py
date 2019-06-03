f4843ef58cbdc89b4681e45f1cc26ce2f4a5c215
magechaP <magechapeter@gmail.com>
2019-06-03 11:46:01 +0300

    fix bugs and finalize on functionality code

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,192 @@ from ..models import User, Post, Comment
 from .forms import UpdateProfile, PostForm , CommentForm
 from .. import db, photos
 app = Flask(__name__)
+
+
+# views
+@main.route("/")
+def index():
+   '''
+   title = "Post Perfect"
+   '''
+   title = 'Post Perfect'
+   posts = Post.query.all()
+
+   return render_template('index.html', title= title, posts = posts)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+    
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
+
+@main.route('/post/new', methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+
+        title=form.title.data
+        content=form.content.data
+        category=form.category.data
+        post = Post(title=title, content=content,category=category)
+        # post.save_post(post)
+        db.session.add(post)
+        db.session.commit()
+
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('main.index', id=post.id))
+
+    return render_template('new_post.html', title='New Post', post_form=form, post ='New Post')
+
+
+@main.route('/comment/new/<int:id>', methods=['GET','POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        
+        comment_content = form.comment.data
+
+        comment = Comment(comment_content= comment_content, post_id=id)
+
+        # post.save_post(post)
+        db.session.add(comment)
+        db.session.commit()
+        
+    comment = Comment.query.filter_by(post_id=id).all()
+    return render_template('new_comment.html', title='New Post', comment=comment,comment_form=form, post ='New Post')
+
+@main.route('/music/new', methods=['GET','POST'])
+@login_required
+def music(category = "Music"):
+
+    musics = Post.query.filter_by(category = "Music")
+    
+    title = "Music Blogs"
+    return render_template('music.html', musics= musics, title=title, post ='New Post')
+
+
+@main.route('/animations/new', methods=['GET','POST'])
+@login_required
+def animations(category = "Animations"):
+
+    animations = Post.query.filter_by(category = "Animations")
+    
+    title = "Animations Blogs"
+    return render_template('animations.html', animations= animations, title=title, post ='New Post')
+
+
+@main.route('/adventure/new', methods=['GET','POST'])
+@login_required
+def adventure(category = "Adventures"):
+
+    adventures = Post.query.filter_by(category = "Adventures")
+    
+    title = "Adventures Blogs"
+    return render_template('adventure.html', adventures= adventures, title=title, post ='New Post')
+
+
+@main.route('/celebrity/new', methods=['GET','POST'])
+@login_required
+def celebrity(category = "Celebrity"):
+
+    celebritys = Post.query.filter_by(category = "Celebrity")
+    
+    title = "Celebrity Blogs"
+    return render_template('celebrity.html', celebritys= celebritys, title=title, post ='New Post')
+
+
+@main.route('/nature/new', methods=['GET','POST'])
+@login_required
+def nature(category = "Nature"):
+
+    natures = Post.query.filter_by(category = "Nature")
+    
+    title = "Nature Blogs"
+    return render_template('nature.html', natures= natures, title=title, post ='New Post')
+
+@main.route('/fashion/new', methods=['GET','POST'])
+@login_required
+def fashion(category = "Fashion"):
+
+    fashions = Post.query.filter_by(category = "Fashion")
+    
+    title = "Fashion Blogs"
+    
+    return render_template('fashion.html', fashions= fashions, title=title, post ='New Post')
+
+@main.route('/frenemies/new', methods=['GET','POST'])
+@login_required
+def frenemies(category = "Fashion"):
+
+    frenemiess = Post.query.filter_by(category = "Frenemies")
+    
+    title = "Frenemies Blogs"
+        
+    return render_template('frenemies.html', frenemiess= frenemiess, title=title, post ='New Post')
+
+
+
+@main.route('/feuds/new', methods=['GET','POST'])
+@login_required
+def feuds(category = "Feuds"):
+
+    feudss = Post.query.filter_by(category = "Feuds")
+    
+    title = "Feuds Blogs"
+    
+    return render_template('feuds.html', feudss= feudss, title=title, post ='New Post')
+
+
+
+@main.route('/newcars/new', methods=['GET','POST'])
+@login_required
+def newcars(category = "New cars"):
+
+    newcarss = Post.query.filter_by(category = "New cars")
+    
+    title = "New cars Blogs"
+    
+    return render_template('newcars.html', newcarss= newcarss, title=title, post ='New Post')
+
+@main.route('/delete/<int:id>',methods=['GET','POST'])
+@login_required
+def delete(id):
+   del_post = Post.query.filter_by(id=id).first()
+   db.session.delete(del_post)
+   db.session.commit()
+   
+   return redirect(url_for('main.index'))
