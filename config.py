62382ed45be7ef130796ea3082a4cff2cf42c769
magechaP <magechapeter@gmail.com>
2019-06-03 10:00:00 +0300

    import os and setup configurations

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY='01570936bd6b231dd1e14cd'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
