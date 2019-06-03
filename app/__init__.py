be7f59810ba80ffad4e6b9ce45ae2fffcb62d5f7
magechaP <magechapeter@gmail.com>
2019-06-03 10:36:00 +0300

    intialize flask extensions and configure uploadset

--- app/__init__.py
+++ app/__init__.py
@@ -19,3 +19,21 @@ def create_app(config_name):
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    db.init_app(app)
+    bootstrap = Bootstrap(app)
+    login_manager.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    # registering the main app blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    return app
